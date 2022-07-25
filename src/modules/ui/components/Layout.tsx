113de32e42a0683eff6b4e625948b3e29506350a
Wutipat Khamnuansin <bellmcp@gmail.com>
2022-07-25 16:16:46 +0700

    Remove unused package

--- src/modules/ui/components/Layout.tsx
+++ src/modules/ui/components/Layout.tsx
@@ -3,18 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading-bar'
-import useKonami from 'use-konami'
-import {
-  CssBaseline,
-  Snackbar,
-  IconButton,
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  Typography,
-  Divider,
-  Link,
-} from '@material-ui/core'
+import { CssBaseline, Snackbar, IconButton } from '@material-ui/core'
 import {
   unstable_createMuiStrictModeTheme as createMuiTheme,
   ThemeProvider,
@@ -118,15 +107,6 @@ export default function Layout() {
     },
   })
 
-  // DEBUG DIALOG
-  useKonami({
-    onUnlock: () => setDebugDialogOpen(true),
-  })
-  const [debugDialogOpen, setDebugDialogOpen] = useState(false)
-  const handleDebugDialogClose = () => {
-    setDebugDialogOpen(false)
-  }
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -142,55 +122,6 @@ export default function Layout() {
       />
       <NavBar active={activePage} setActivePage={setActivePage} />
       <Routes />
-      {/* DEBUG DIALOG */}
-      <Dialog open={debugDialogOpen} onClose={handleDebugDialogClose}>
-        <DialogTitle onClose={handleDebugDialogClose}>เกี่ยวกับ</DialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom style={{ fontWeight: 600 }} variant='body1'>
-            OCSC LEARNING SPACE (LEARNER PART)
-            <br />
-            Version 2.1.2
-          </Typography>
-          <Divider style={{ margin: '16px 0' }} />
-          <Typography gutterBottom style={{ fontWeight: 600 }}>
-            Developer
-          </Typography>
-          <Typography gutterBottom variant='body2'>
-            WUTIPAT KHAMNUANSIN (Front-End)
-            <br />
-            ANUNYA PRASONGKIAT (Back-End)
-          </Typography>
-          <Divider style={{ margin: '16px 0' }} />
-          <Typography gutterBottom style={{ fontWeight: 600 }}>
-            Advisor
-          </Typography>
-          <Typography gutterBottom variant='body2'>
-            ASSOC. PROF. CHATCHAWIT APORNTEWAN, Ph.D.
-          </Typography>
-          <Divider style={{ margin: '16px 0' }} />
-          <Typography gutterBottom variant='caption' color='textSecondary'>
-            Licensed under{' '}
-            <Link
-              href='https://github.com/bellmcp/OCSC-Learning-Platform/blob/master/LICENSE'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              The GNU General Public License v3.0 License
-            </Link>
-            .
-            <br />
-            Copyright © {new Date().getFullYear()}{' '}
-            <Link
-              href='https://www.ocsc.go.th/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Office of the Civil Service Commission (OCSC)
-            </Link>
-            , All rights reserved.
-          </Typography>
-        </DialogContent>
-      </Dialog>
       <Snackbar
         open={isSnackbarOpen}
         onClose={closeFlashMessage}
